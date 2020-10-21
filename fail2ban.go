72683b340ed784a194049e279150af2a2fcde44e
Tom Moulard <tom.moulard@epita.fr>
2020-10-21 15:56:44 +0200

    fail2ban: fixing mixup

--- fail2ban.go
+++ fail2ban.go
@@ -5,36 +5,29 @@ import (
 	"net/http"
 )
 
-// struct fail2ban config 
-type rules struct{	
-	ignorecommand 			string 		`yaml:"igonecommand"`
-	bantime 			int64		`yaml:"bantime"` 			//exprimate in second
-	findtime			int64		`yaml:"findtime"`			//exprimate in second
-	maxretry			int		`yaml:"maxretry"`		
-	backend				string		`yaml:"backend"`			//maybe we have to change this to another things or just delete it if its useless
-	usedns				string		`yaml:"usedns"`				//maybe change string by a int for limit the size (yes:0, warn:1, no:2, raw:3)
-	logencoding 			string		`yaml:"logencoding"`			//maybe useless for our project (utf-8, ascii)	
-	enabled 			bool		`yaml:"enabled"`			//enable or disable the jail
-	mode 				string		`yaml:"mode"`				//same than usedns
-	filter				string		`yaml:"filter"` 			//= %(name)s[mode=%(mode)s] maybe change for a []string
-	destemail			string		`yaml:"destemail"` 	
-	sender				string		`yaml:"sender"`	
-	mta				string		`yaml:"mta"`				//same than usedns
-	protocol			string		`yaml:"protocol"`  			//maybe int (tcp:0, udp:1)
-	chain 				string		`yaml:"chain"`				//maybe useless because handle by traefik chain
-	port 				[2]int		`yaml:"port"`				//i dont know if yaml can take this
-<<<<<<< HEAD
-	fail2banAgent 		string		`yaml:"fail2ban_agent"`		
-	banaction 			string		`yaml:"banaction_allports"`	//maybe useless because we are the firewall ?
-	banactionAllports 	string		`yaml:"banaction_allports"` //same as above
-	actionAbuseipdb 	string		`yaml:"action_abuseipdb"`	
-=======
-	fail2ban_agent 			string		`yaml:"fail2ban_agent"`		
-	banaction 			string		`yaml:"banaction_allports"`		//maybe useless because we are the firewall ?
-	banaction_allports 		string		`yaml:"banaction_allports"` 		//same as above
-	action_abuseipdb 		string		`yaml:"action_abuseipdb"`	
->>>>>>> c199e7c0039628422a0c499ad28ce6627af4b9d0
-	action 				string		`yaml:"action"`				//maybe change for []string
+// struct fail2ban config
+type rules struct {
+	ignorecommand     string `yaml:"igonecommand"`
+	bantime           int64  `yaml:"bantime"`  //exprimate in second
+	findtime          int64  `yaml:"findtime"` //exprimate in second
+	maxretry          int    `yaml:"maxretry"`
+	backend           string `yaml:"backend"`     //maybe we have to change this to another things or just delete it if its useless
+	usedns            string `yaml:"usedns"`      //maybe change string by a int for limit the size (yes:0, warn:1, no:2, raw:3)
+	logencoding       string `yaml:"logencoding"` //maybe useless for our project (utf-8, ascii)
+	enabled           bool   `yaml:"enabled"`     //enable or disable the jail
+	mode              string `yaml:"mode"`        //same than usedns
+	filter            string `yaml:"filter"`      //= %(name)s[mode=%(mode)s] maybe change for a []string
+	destemail         string `yaml:"destemail"`
+	sender            string `yaml:"sender"`
+	mta               string `yaml:"mta"`      //same than usedns
+	protocol          string `yaml:"protocol"` //maybe int (tcp:0, udp:1)
+	chain             string `yaml:"chain"`    //maybe useless because handle by traefik chain
+	port              [2]int `yaml:"port"`     //i dont know if yaml can take this
+	fail2banAgent     string `yaml:"fail2ban_agent"`
+	banaction         string `yaml:"banaction_allports"` //maybe useless because we are the firewall ?
+	banactionAllports string `yaml:"banaction_allports"` //same as above
+	actionAbuseipdb   string `yaml:"action_abuseipdb"`
+	action            string `yaml:"action"` //maybe change for []string
 }
 
 // Config holds configuration to be passed to the plugin
