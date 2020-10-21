1423b996e26cf03bb150d791183c9f405e7c7681
Alexandre Bossut-Lasry <alexandre.bossut-lasry@epita.fr>
2020-10-21 18:48:18 +0200

    Adding ipChecking lib with tests
    Still some problems on CIDR checking

---
+++ ipChecking/ipChecking_test.go
@@ -0,0 +1,62 @@
+package ipChecking_test
+
+import (
+	"testing"
+
+	ipChecking "github.com/tommoulard/fail2ban/ipChecking"
+)
+
+func TestIpChecking(t *testing.T) {
+	tests := []struct {
+		name     string
+		stringIp string
+		testedIp ipChecking.Ip
+		res      bool
+	}{
+		{
+			name:     "[IP] No match",
+			stringIp: "127.0.0.1",
+			testedIp: ipChecking.Ip{
+				Ip:   (127 << 24) + 1,
+				Cidr: 0xFFFFFFFF,
+			},
+			res: true,
+		},
+		{
+			name:     "[IP] Match",
+			stringIp: "127.0.0.1",
+			testedIp: ipChecking.Ip{
+				Ip:   (127 << 24) + 2,
+				Cidr: 0xFFFFFFFF,
+			},
+			res: false,
+		},
+		{
+			name:     "[CIDR] No Match",
+			stringIp: "127.0.0.1",
+			testedIp: ipChecking.Ip{
+				Ip:   (127 << 24) + 16 + 1,
+				Cidr: 0xFFFFFF00,
+			},
+			res: false,
+		},
+		{
+			name:     "[CIDR] Match",
+			stringIp: "127.0.0.1",
+			testedIp: ipChecking.Ip{
+				Ip:   (127 << 24) + 1,
+				Cidr: 0xFFFFFF00,
+			},
+			res: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := tt.testedIp.CheckIpInSubnet(tt.stringIp)
+			if r != tt.res {
+				t.Errorf("wanted '%v' got '%v'", tt.res, r)
+			}
+		})
+	}
+}
