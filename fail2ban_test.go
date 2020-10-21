4f225ca971825cca1e3f5246cec2a0de7e815aca
Martin <martin.huvelle@epita.fr>
2020-10-21 17:33:13 +0200

    fail2ban: add tests

--- fail2ban_test.go
+++ fail2ban_test.go
@@ -10,3 +10,33 @@ func TestDummy(t *testing.T) {
 	cfg := plug.CreateConfig()
 	t.Log(cfg)
 }
+
+func TestImportIp(t *testing.T) {
+    tests := []struct {
+		name    string
+        list    plug.List
+        strWant []string
+        err     error
+	}{
+        {
+            name: "empty list",
+            list: plug.List{
+				Ip:   []string{},
+				Files: []string{},
+			},
+            strWant: []string {},
+            err: nil,
+        },
+    }
+    for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+            got, e := plug.ImportIP (tt.list)
+            if e != tt.err {
+                t.Errorf("wanted '%s' got '%s'", tt.err, e)
+            }
+            if len(got) != len(tt.strWant) {
+                t.Errorf("wanted '%s' got '%s'", tt.strWant, got)
+            }
+        })
+    }
+}
