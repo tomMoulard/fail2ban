5d43cc306176d1bb3e3049ae9a125aab745252e2
Clement David <clement.david@epita.fr>
2021-01-31 21:22:44 +0100

    [CI test] change commands in CI script

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,8 +2,8 @@
 
 docker run -d --network host containous/whoami -port 5000
 
-curl -O https://github.com/traefik/traefik/releases/download/v2.3.6/traefik_v2.3.6_linux_amd64.tar.gz
+curl -L -O https://github.com/traefik/traefik/releases/download/v2.3.6/traefik_v2.3.6_linux_amd64.tar.gz
 tar -zxvf traefik_v2.3.6_linux_amd64.tar.gz
 
-./traefik --configfile trafik.yaml
+./traefik --configfile scripts/traefik-ci.yaml
 
