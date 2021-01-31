c89643d257b087bceda843a219abbb5d7635d820
Clement David <clement.david@epita.fr>
2021-02-01 00:25:54 +0100

    [CI test] add -v and change trafik-ci

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -v
+
 docker run -d --network host containous/whoami -port 5000
 
 curl -L -O https://github.com/traefik/traefik/releases/download/v2.3.6/traefik_v2.3.6_linux_amd64.tar.gz
