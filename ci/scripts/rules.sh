25e8d6cee728299f2fc0835960f20ec620eeef5a
Clement David <clement.david@epita.fr>
2021-02-01 14:23:17 +0100

    [CI test] add echo "timeout" and -e option

--- ci/scripts/rules.sh
+++ ci/scripts/rules.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -v
+set -ev
 
 echo "########### $1 # START ##############"
 
 sed "/filename:/ s;$; ci/yamls/$1.yaml;" "ci/yamls/traefik-ci.yaml" > ci/inside_ci/ci-$1.yaml
 
-timeout 20s ./traefik --configfile ci/inside_ci/ci-$1.yaml 1> ci/inside_ci/logs.all &
+timeout 20s ./traefik --configfile ci/inside_ci/ci-$1.yaml 1> ci/inside_ci/logs.all || echo 'timeout traefik' &
 
 sleep 5
 
