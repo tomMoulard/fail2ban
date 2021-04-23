6efd782377d2b7aceb4769cee0d2d39c2fd55c15
Clément David <clement.david@epita.fr>
2021-04-23 21:40:10 +0200

    change logs in CI

--- ci/scripts/check-local-banned.sh
+++ ci/scripts/check-local-banned.sh
@@ -3,4 +3,4 @@ set -ev
 
 grep "Blacklisted: '127.0.0.1/32'" ci/inside_ci/logs.all && echo 'OK'
 
-grep "127.0.0.1 is in blacklisted" ci/inside_ci/logs.all && echo 'OK'
+grep "127.0.0.1 is now banned temporarily" ci/inside_ci/logs.all && echo 'OK'
--- ci/scripts/check-local-whited.sh
+++ ci/scripts/check-local-whited.sh
@@ -3,4 +3,4 @@ set -ev
 
 grep "Whitelisted: '127.0.0.1/32'" ci/inside_ci/logs.all && echo 'OK'
 
-grep "127.0.0.1 is in blacklist mode" ci/inside_ci/logs.all || echo 'OK'
+grep "127.0.0.1 is now banned temporarily" ci/inside_ci/logs.all || echo 'OK'
