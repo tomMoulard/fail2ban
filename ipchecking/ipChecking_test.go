bbac88b5713e17aa57a5f3f68f32c288796b17a2
Tom Moulard <tom@moulard.org>
2023-09-29 23:23:38 +0200

    lint: dupl

--- ipchecking/ipChecking_test.go
+++ ipchecking/ipChecking_test.go
@@ -6,7 +6,7 @@ import (
 	ipchecking "github.com/tomMoulard/fail2ban/ipchecking"
 )
 
-func TestIPv4Generation(t *testing.T) {
+func TestIPGeneration(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
@@ -59,28 +59,6 @@ func TestIPv4Generation(t *testing.T) {
 			stringIP: "127.0.0.1/23",
 			res:      true,
 		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			_, err := ipchecking.BuildIP(tt.stringIP)
-			if (err != nil) == tt.res {
-				t.Errorf("wanted '%v' got '%v'", tt.res, (err == nil))
-			}
-		})
-	}
-}
-
-func TestIPv6Generation(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name     string
-		stringIP string
-		res      bool
-	}{
 		{
 			name:     "[IP] Valid IPv6",
 			stringIP: "::1",
@@ -140,28 +118,29 @@ func TestIPv6Generation(t *testing.T) {
 	}
 }
 
-func TestIPv4Checking(t *testing.T) {
+func helpBuildIP(t *testing.T, ip string) ipchecking.IP {
+	t.Helper()
+
+	nip, err := ipchecking.BuildIP(ip)
+	if err != nil {
+		t.Errorf("Error in IP building: %s, with err %v", nip, err)
+	}
+
+	return nip
+}
+
+func TestIPChecking(t *testing.T) {
 	t.Parallel()
 
-	ip, err := ipchecking.BuildIP("127.0.0.1")
-	if err != nil {
-		t.Errorf("Error in IPv4 building: %s, with err %v", "127.0.0.1", err)
-	}
+	ipv4 := helpBuildIP(t, "127.0.0.1")
+	ipv42 := helpBuildIP(t, "127.0.0.2")
+	cidrv41 := helpBuildIP(t, "127.0.0.1/24")
+	cidrv42 := helpBuildIP(t, "127.0.1.1/24")
 
-	ip2, err := ipchecking.BuildIP("127.0.0.2")
-	if err != nil {
-		t.Errorf("Error in IPv4 building: %s, with err %v", "127.0.0.1", err)
-	}
-
-	cidr1, err := ipchecking.BuildIP("127.0.0.1/24")
-	if err != nil {
-		t.Errorf("Error in IPv4 building: %s, with err %v", "127.0.0.1", err)
-	}
-
-	cidr2, err := ipchecking.BuildIP("127.0.1.1/24")
-	if err != nil {
-		t.Errorf("Error in IPv4 building: %s, with err %v", "127.0.0.1", err)
-	}
+	ipv6 := helpBuildIP(t, "::1")
+	ipv62 := helpBuildIP(t, "::2")
+	cidrv61 := helpBuildIP(t, "::1/124")
+	cidrv62 := helpBuildIP(t, "::1:1/124")
 
 	tests := []struct {
 		name     string
@@ -172,92 +151,49 @@ func TestIPv4Checking(t *testing.T) {
 		{
 			name:     "[IP] IPv4 match",
 			stringIP: "127.0.0.1",
-			testedIP: ip,
+			testedIP: ipv4,
 			res:      true,
 		},
 		{
 			name:     "[IP] IPv4 No Match",
 			stringIP: "127.0.0.1",
-			testedIP: ip2,
+			testedIP: ipv42,
 			res:      false,
 		},
 		{
 			name:     "[CIDR] IPv4 No Match",
 			stringIP: "127.0.0.1",
-			testedIP: cidr2,
+			testedIP: cidrv42,
 			res:      false,
 		},
 		{
 			name:     "[CIDR] IPv4 Match",
 			stringIP: "127.0.0.1",
-			testedIP: cidr1,
+			testedIP: cidrv41,
 			res:      true,
 		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			r := tt.testedIP.CheckIPInSubnet(tt.stringIP)
-			if r != tt.res {
-				t.Errorf("wanted '%v' got '%v'", tt.res, r)
-			}
-		})
-	}
-}
-
-func TestIPv6Checking(t *testing.T) {
-	t.Parallel()
-
-	ip, err := ipchecking.BuildIP("::1")
-	if err != nil {
-		t.Errorf("Error in IP building: %s, with err %v", "::1", err)
-	}
-
-	ip2, err := ipchecking.BuildIP("::2")
-	if err != nil {
-		t.Errorf("Error in IP building: %s, with err %v", "::1", err)
-	}
-
-	cidr1, err := ipchecking.BuildIP("::1/124")
-	if err != nil {
-		t.Errorf("Error in IP building: %s, with err %v", "::1/24", err)
-	}
-
-	cidr2, err := ipchecking.BuildIP("::1:1/124")
-	if err != nil {
-		t.Errorf("Error in IP building: %s, with err %v", "::1:1/24", err)
-	}
-
-	tests := []struct {
-		name     string
-		stringIP string
-		testedIP ipchecking.IP
-		res      bool
-	}{
 		{
 			name:     "[IP] IPv6 match",
 			stringIP: "::1",
-			testedIP: ip,
+			testedIP: ipv6,
 			res:      true,
 		},
 		{
 			name:     "[IP] IPv6 No Match",
 			stringIP: "::1",
-			testedIP: ip2,
+			testedIP: ipv62,
 			res:      false,
 		},
 		{
 			name:     "[CIDR] IPv6 No Match",
 			stringIP: "::1",
-			testedIP: cidr2,
+			testedIP: cidrv62,
 			res:      false,
 		},
 		{
 			name:     "[CIDR] IPv6 Match",
 			stringIP: "::1",
-			testedIP: cidr1,
+			testedIP: cidrv61,
 			res:      true,
 		},
 	}
