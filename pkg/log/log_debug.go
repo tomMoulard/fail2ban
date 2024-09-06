e65087924e7adf3af6399d24c2f3e9c266f079d3
Tom Moulard <tom.moulard@perfectstay.com>
2024-09-06 21:17:10 +0200

    chore: enalbing logger temporarily

--- pkg/log/log_debug.go
+++ pkg/log/log_debug.go
@@ -6,13 +6,16 @@ package log
 
 import (
 	"log"
-	"os"
 )
 
 type Logger struct {
 	*log.Logger
 }
 
-func New(prefix string) Logger {
-	return Logger{log.New(os.Stdout, prefix+": ", log.Ldate|log.Ltime|log.Lshortfile)}
+func New(string) Logger {
+	return Logger{}
 }
+
+func (l Logger) Printf(format string, v ...interface{}) {}
+
+func (l Logger) Println(v ...interface{}) {}
