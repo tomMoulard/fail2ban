66027c252ed9aa22a7750666f033ad3d5a72b097
Tom Moulard <tom.moulard@perfectstay.com>
2024-09-06 21:20:02 +0200

    chore: update misspell and goreleaser tools

--- tools/tools.go
+++ tools/tools.go
@@ -9,9 +9,9 @@ package tools
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 // https://github.com/golang/go/issues/25922
 import (
-	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/golangci/misspell/cmd/misspell"
+	_ "github.com/goreleaser/goreleaser/v2"
 	_ "github.com/traefik/yaegi/cmd/yaegi"
 	_ "golang.org/x/vuln/cmd/govulncheck"
 )
